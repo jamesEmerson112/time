96c7d923eebd52470bbb8da1f3a1f01a141dc23d
jamesEmerson112 <36806380+jamesEmerson112@users.noreply.github.com>
2022-09-28 13:45:26 -0700

    Display some squares

--- src/Components/DashboardComponents/ReportComponent.js
+++ src/Components/DashboardComponents/ReportComponent.js
@@ -1,19 +1,21 @@
 import React from "react";
+import {BsFillSquareFill} from "react-icons/bs";
 
 export default function ReportComponent () {
-  let text = "";
-  for (let i = 0; i < 4383; i += 1) {
-    text += '. ';
-  }
+  // let text = "";
+  // for (let i = 0; i < 4383; i += 1) {
+  //   text += '. ';
+  // }
 
   return(
     <div className="ReportComponent">
       <p>Report info</p>
       <div>
         {[...Array(4383)].map((x, index) => {
-          return <div key={index} className='square'> TEST </div>
+          return <div key={index}> TEST <BsFillSquareFill /></div>
         })}
       </div >
     </div>
   )
 }
+
