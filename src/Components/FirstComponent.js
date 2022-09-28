d83fced7b73bdab640706ea8b358efb55e9fba08
jamesEmerson112 <36806380+jamesEmerson112@users.noreply.github.com>
2022-09-27 23:39:54 -0700

    First form

---
+++ src/Components/FirstComponent.js
@@ -0,0 +1,28 @@
+import './FirstComponent.css';
+
+function FirstComponent() {
+  const pressSubmit = (e) => {
+    e.preventDefault();
+    console.log('hello');
+  }
+
+  return (
+    <div className="FirstComponent">
+       {/* <form className="form" action="/" method="post"> */}
+       <form className="form" >
+        <h2>CREATE AN ENTRY</h2>
+        <p type="Number"><input type="text" name="age" placeholder="Enter your age here."></input></p>
+
+        <p type="Number"><input type="text" name="weeklyWorkHrs" placeholder="Enter your daily work hours"></input></p>
+
+        <p type="Number"><input type="text" name="dailyChoresHrs" placeholder="Enter your daily chore hours"></input></p>
+
+        <p type="Number"><input type="text" name="dailyChoresHrs" placeholder="Enter your daily transportation hours"></input></p>
+
+        <input className="button" type="submit" value="Submit" onClick={pressSubmit}/>
+      </form>
+    </div>
+  );
+}
+
+export default FirstComponent;
