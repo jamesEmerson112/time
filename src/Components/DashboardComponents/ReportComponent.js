bd951cea4307a9a5419fc60621bbdd032a6492fa
jamesEmerson112 <36806380+jamesEmerson112@users.noreply.github.com>
2022-09-28 14:30:54 -0700

    Array run out of stack. Attempting with Linked List

--- src/Components/DashboardComponents/ReportComponent.js
+++ src/Components/DashboardComponents/ReportComponent.js
@@ -1,18 +1,38 @@
 import React from "react";
 import {BsFillSquareFill} from "react-icons/bs";
+import '../DashboardComponent.css';
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+}
+
+
+
 
 export default function ReportComponent () {
-  // let text = "";
-  // for (let i = 0; i < 4383; i += 1) {
-  //   text += '. ';
-  // }
+  let test = [];
+  for (let i = 0; i < 4343; i += 1) {
+    test.push(<BsFillSquareFill key={i}></BsFillSquareFill>);
+  }
+
+  console.log(test);
 
   return(
     <div className="ReportComponent">
       <p>Report info</p>
-      <div>
-        {[...Array(4383)].map((x, index) => {
-          return <div key={index}> TEST <BsFillSquareFill /></div>
+      <div className="ReportComponent Test">
+        {test.map((item, index) => {
+          return item;
         })}
       </div >
     </div>
